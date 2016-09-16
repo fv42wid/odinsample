a1ec1ca7615a47720fe277f4779a679a10561741
Frank Vinci <fv42wid@yahoo.com>
2016-09-16 23:08:14 +0000

    recommit microposts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :users
+  resources :microposts, only: [:create, :destroy]
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
