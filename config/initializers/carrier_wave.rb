1d51daaabc836517d6ba38fc9004d01f319d5379
Frank Vinci <fv42wid@yahoo.com>
2016-09-17 16:20:04 +0000

    updated carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
       #config for amazon s3
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region => ENV['S3_REGION']
     }
     config.fog_directory = ENV['S3_BUCKEY']
