b5bb9ca0f91f4cdb88705afcf5dff34b6c6ee337
Frank Vinci <fv42wid@yahoo.com>
2016-09-19 13:24:03 +0000

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,16 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
